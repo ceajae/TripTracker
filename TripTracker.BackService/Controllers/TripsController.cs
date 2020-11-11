7cc649a14f779973001d43e8d357f7e60606f704
chijioke.molokwu <chijioke.molokwu@interswitchgroup.com>
2020-11-11 20:47:41 +0100

    First module

---
+++ TripTracker.BackService/Controllers/TripsController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TripTracker.BackService.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TripTracker.BackService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TripsController : ControllerBase
+    {
+        public TripsController(Repository repository)
+        {
+            _repository = repository;
+        }
+
+        private Repository _repository;
+
+        // GET: api/<TripsController>
+        [HttpGet]
+        public IEnumerable<Trip> Get() 
+        {
+            return _repository.Get(); 
+        }
+
+        // GET api/<TripsController>/5
+        [HttpGet("{id}")]
+        public Trip Get(int id)
+        {
+            return _repository.Get(id);
+        }
+
+        // POST api/<TripsController>
+        [HttpPost]
+        public void Post([FromBody] Trip value)
+        {
+            _repository.Add(value);
+        }
+
+        // PUT api/<TripsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Trip value)
+        {
+            _repository.Update(value);
+        }
+
+        // DELETE api/<TripsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _repository.Remove(id);
+        }
+    }
+}
---
+++ TripTracker.BackService/Startup.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace TripTracker.BackService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<Models.Repository>();
+            services.AddControllers();
+
+            services.AddSwaggerGen(options => 
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "Trip Tracker", Version = "v1" })
+            );
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(options =>
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Trip Tracker v1")
+            );
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
